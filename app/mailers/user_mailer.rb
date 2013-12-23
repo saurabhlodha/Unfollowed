95ca66bf94cafd641b2a54650cce04b06261e325
saurabhlodha <saurabhlodha11@gmail.com>
2013-12-24 02:17:06 +0530

    Sendgrid Configure Changes-3

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "SaurabhLodha@knowWhoUnfollowed"
 
 def welcome_email(h, unfollowed, e, nh,newFollowed)
     @hash = h
